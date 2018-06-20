be563b03e9843e8670b5f7bf0dad047b313ae231
Sunny1987 <sab.roy.87@gmail.com>
2018-06-20 08:16:44 +0530

    After Sell item updation, an item summary is displayed

--- ParseStarterProject/src/main/java/com/parse/starter/OrderSummary.java
+++ ParseStarterProject/src/main/java/com/parse/starter/OrderSummary.java
@@ -4,6 +4,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -48,11 +49,13 @@ public class OrderSummary extends AppCompatActivity {
                             book_price.setText(parseObject.getString("price"));
 
                             ParseFile file = (ParseFile) parseObject.get("image_book");
+
                             file.getDataInBackground(new GetDataCallback() {
                                 @Override
                                 public void done(byte[] data, ParseException e) {
                                     if(e== null && data != null){
                                         Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+                                        Log.i("bitmap",bitmap.toString());
                                         book_pic.setImageBitmap(bitmap);
                                     }
                                 }
