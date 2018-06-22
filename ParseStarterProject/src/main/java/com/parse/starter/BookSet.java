8d169d756044c82def4911923f20db5787f3bd93
Sunny1987 <sab.roy.87@gmail.com>
2018-06-22 08:39:15 +0530

    Context variable

--- ParseStarterProject/src/main/java/com/parse/starter/BookSet.java
+++ ParseStarterProject/src/main/java/com/parse/starter/BookSet.java
@@ -1,5 +1,6 @@
 package com.parse.starter;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 
 import com.parse.ParseFile;
@@ -9,15 +10,34 @@ import java.util.BitSet;
 
 public class BookSet {
 
-    private String book_name,book_cat,book_pub,book_price;
+    private String book_name,book_cat,book_pub,book_price,seller;
     private ParseFile book_image;
+    private Context context;
 
-    public BookSet(String book_name, String book_cat, String book_pub, String book_price, ParseFile book_image) {
+    public BookSet(String book_name, String book_cat, String book_pub, String book_price, ParseFile book_image,String seller,Context context) {
         this.book_name = book_name;
         this.book_cat = book_cat;
         this.book_pub = book_pub;
         this.book_price = book_price;
         this.book_image = book_image;
+        this.seller = seller;
+        this.context = context;
+    }
+
+    public Context getContext(){
+        return context;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    public String getSeller() {
+        return seller;
+    }
+
+    public void setSeller(String seller) {
+        this.seller = seller;
     }
 
     public String getBook_name() {
